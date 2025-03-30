676d7169ad86952ef992baa1fd79c31797b8726a
luis4383 <luisapbc@hotmail.com>
2025-03-30 23:21:32 +0000

    Total

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,30 +4,34 @@
 
 int main() {
     char estado01, estado02;
-    char codigodacarta01[20], codigodacarta02[20];
-    char cidade01[50], cidade02[50];
-    int populacao01, populacao02;
+    char codigodacarta01, codigodacarta02;
+    char cidadeA, cidadeB;
+    int  populacao01, populacao02;
     float area01, area02;
     float PIB01, PIB02;
     int num_pontos_tur_01, num_pontos_tur_02;
 
 
     printf("Digite o estado 01:\n");
-    scanf("%c", &estado01);
+    scanf("%c",&estado01);
 
     printf("Digite o codigo da carta01:\n");
-    scanf("%s",codigodacarta01);
+    scanf("%d",&codigodacarta01);
 
-    printf("Digite a cidade01:\n");
-    scanf("%e",&cidade01);
-    
+   printf("qual a cidade:\n");
+    scanf("%d",&cidadeA);
 
+    printf("qual a populacao01:\n");
+    scanf("%s",&populacao01);
 
+    printf("tamanho da area:\n");
+    scanf("%f",&area01);
 
+    printf("qual o pib01:\n");
+    scanf("%f",&PIB01);
 
-
-
-
+    printf("numeros de pontos turisticos:\n");
+    scanf("%d",&num_pontos_tur_01 );
 
 
 
