ed4e191b8d22a06f77599ce1fdded020c0993692
luis4383 <luisapbc@hotmail.com>
2025-03-31 02:00:33 +0000

    jogo super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,34 +4,68 @@
 
 int main() {
     char estado01, estado02;
-    char codigodacarta01, codigodacarta02;
-    char cidadeA, cidadeB;
+    int codigodacarta01, codigodacarta02;
+    char cidadeA, cidadeB[50];
     int  populacao01, populacao02;
     float area01, area02;
     float PIB01, PIB02;
     int num_pontos_tur_01, num_pontos_tur_02;
 
 
-    printf("Digite o estado 01:\n");
+    printf("Digite a letra do  estado01:\n");
     scanf("%c",&estado01);
 
     printf("Digite o codigo da carta01:\n");
     scanf("%d",&codigodacarta01);
 
-   printf("qual a cidade:\n");
-    scanf("%d",&cidadeA);
+   printf("qual a cidade do estado01:\n");
+    scanf("%s",&cidadeA);
 
     printf("qual a populacao01:\n");
-    scanf("%s",&populacao01);
+    scanf("%d",&populacao01);
 
-    printf("tamanho da area:\n");
+    printf("tamanho da area01:\n");
     scanf("%f",&area01);
 
     printf("qual o pib01:\n");
     scanf("%f",&PIB01);
 
-    printf("numeros de pontos turisticos:\n");
+    printf("numeros de pontos turisticos01:\n");
     scanf("%d",&num_pontos_tur_01 );
+
+
+
+
+
+    printf("digite a letra do estado02:\n");
+    scanf(" %c",&estado02);
+
+    printf("digite o codigo da carta02:\n");
+    scanf("%d",&codigodacarta02);
+
+    printf("qual a cidade B: \n");
+    scanf ("%s",&cidadeB);
+
+     printf ("qual a populacao da cidadeB\n");
+    scanf ("%d",&populacao02);
+
+    printf ("qual o tamanho da area2:\n");
+    scanf ("%f",&area02);
+
+    printf("qual o pib02:\n");
+    scanf("%f",&PIB02);
+
+    printf ("quantidade de pontos turisticos02:\n");
+    scanf ("%d",&num_pontos_tur_02);
+
+
+
+
+
+
+
+
+
 
 
 
